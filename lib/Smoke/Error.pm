63a9cd36d947d0e3134fc4a2f903c7b0e05f8a4d
mp0liiu <raian@reeshome.org>
2018-07-05 13:52:18 +0900

    stack_trace -> cause & longmess

--- lib/Smoke/Error.pm
+++ lib/Smoke/Error.pm
@@ -2,12 +2,47 @@ package Smoke::Error {
 
   use Mouse;
   use Smoke;
+  use Carp ();
 
   has cause => (
     is       => 'ro',
     isa      => 'Str',
     required => 1,
   );
+
+  has longmess => (
+    is      => 'ro',
+    isa     => 'Str',
+    builder => '_build_longmess',  
+  );
+
+  has stack_trace => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_build_stack_trace',
+  );
+
+  sub _build_longmess($self) {
+    Carp::longmess();
+  }
+
+  sub _build_stack_trace($self) {
+    $self->cause . $self->longmess;
+  }
+
+  sub description($self) {
+    my $template = <<EOS
+@@ @{[ ref $self ]} @@
+
+cause : @{[ $self->cause ]}
+@{[ $self->optional_description ]}
+stack_trace : 
+@{[ $self->stack_trace ]}
+EOS
+  }
+
+  sub optional_description($self) {}
 
   __PACKAGE__->meta->make_immutable;
 
